caaf4d8b6d467d7a4452654e3c1cf143807d08c6
Nick Carroll <nicholascarroll@MacBook-Pro.local>
2016-03-20 19:13:48 +0000

    0.2.1
    Fixed navbar for ipad.
    Fixed links and scrolling.
    Added contact content.

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,6 @@
 'use strict';
 
 $(document).ready(function() {
-  $('.btn').on('click', function() {
-  	var $this = $(this);
-  	if($this.hasClass('btn-default')){
-    	$this.removeClass('btn-default').addClass('btn-success');
-    	$this.append('<span class="glyphicon glyphicon-apple"></span>');
-    }
-    else if($this.hasClass('btn-success')){
-    	$this.removeClass('btn-success').addClass('btn-default');
-    	$this.find('.glyphicon').remove();
-    }
-  });
-
-  $('.replace').on('click', function() {
-  	var button = $(this);
-  	if(button.hasClass('two')){
-		var message = $('<p>This is replacement text 2.0!!!</p>');
-  	}
-  	else{
-    	var message = $('<p>This is replacement text!!!</p>');
-    }
-    var $this = $(this).closest('.col-xs-4');
-    $this.find('p').remove();
-    $(message).insertBefore($this.find('.btn'));
-  });
 
   $('.about').on('click', function(event){
     event.stopPropagation(); //stops event from traversing up the DOM
@@ -33,13 +9,24 @@ $(document).ready(function() {
 
   });
 
-$('a').click(function(){
+  $('.btn').on('click', function(){
+    var $this = $(this);
+    if($this.hasClass("btn-phone")){
+      $this.parent().find('.email').slideUp();
+      $this.parent().find('.phone').slideToggle();
+    }
+    else if($this.hasClass("btn-email")){
+      $this.parent().find('.phone').slideUp();
+      $this.parent().find('.email').slideToggle();
+    }
+  });
+
+$('nav a').click(function(){
     $('html, body').animate({
         scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
     }, 500);
     return false;
 });
-
 
 
 
