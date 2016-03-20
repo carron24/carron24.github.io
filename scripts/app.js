5bd0fbbc4eeacb11ab4ab8f840f757651bd1fe54
Nick Carroll <nicholascarroll@MacBook-Pro.local>
2016-03-20 21:19:56 +0000

    0.2.2
    Carousel implemented.
    Example photos added.

--- scripts/app.js
+++ scripts/app.js
@@ -29,7 +29,20 @@ $('nav a').click(function(){
 });
 
 
+$("#myCarousel").carousel();
 
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
+$(".right").click(function(){
+    $("#myCarousel").carousel("next");
+});
 
 $(function() { $('body').hide().show(); });
 
